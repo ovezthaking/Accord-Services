0e10bdd2a3a7379ef48dc23fee67bdd6c640de32
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-15 02:55:13 +0100

    layout settings

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Space_Grotesk } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
 
