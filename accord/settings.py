8dee528c4888a05e3ebeff0dd23945ac504607e8
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-17 02:47:38 +0100

    correctly add apps to settings

--- accord/settings.py
+++ accord/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'contact',
-    'chatai'
+    'contact.apps.ContactConfig',
+    'chatai.apps.ChataiConfig'
 ]
 
 MIDDLEWARE = [
