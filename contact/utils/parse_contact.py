dd07043198d84afe777e5dd86ea958ab409c7e33
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-21 14:49:58 +0100

    Refactor parse_contact function for improved readability

--- contact/utils/parse_contact.py
+++ contact/utils/parse_contact.py
@@ -1,2 +1,4 @@
 def parse_contact(request) -> dict:
-    return request.data if isinstance(request.data, dict) else request.data.dict()
+    if isinstance(request.data, dict):
+        return request.data
+    return request.data.dict()
