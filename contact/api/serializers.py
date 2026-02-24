d283af50731a470b8a6b0048450a9eba186c5f38
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-25 00:42:32 +0100

    Add status field to ContactSerializer with SlugRelatedField

--- contact/api/serializers.py
+++ contact/api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework.serializers import ModelSerializer
-from contact.models import Contact
+from rest_framework.relations import SlugRelatedField
+from contact.models import Contact, ContactStatus
 
 
 class ContactSerializer(ModelSerializer):
+    status = SlugRelatedField(
+        queryset=ContactStatus.objects.all(),
+        slug_field='name'
+    )
+
     class Meta:
         model = Contact
-        fields = '__all__'
+        fields = (
+            'id',
+            'full_name',
+            'status',
+            'phone_number',
+            'email',
+            'services',
+            'description',
+            'createdAt'
+        )
