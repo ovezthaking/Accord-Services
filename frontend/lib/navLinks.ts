683382a11280957f103ce5f5f87c0dcccfdd8df8
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-02-26 03:14:05 +0100

    Change hrefs to absolute main page hrefs for service pages to handle it

--- frontend/lib/navLinks.ts
+++ frontend/lib/navLinks.ts
@@ -1,9 +1,9 @@
 import type { LinksArrayType } from "./types"
 
 export const navLinks: LinksArrayType = [
-    { label: "Strona Główna", href: "#" },
-    { label: "Usługi", href: "#uslugi" },
-    { label: "O Nas", href: "#o-nas" },
-    { label: "Realizacja", href: "#realizacja" },
-    { label: "Kontakt", href: "#kontakt" },
+    { label: "Strona Główna", href: "/" },
+    { label: "Usługi", href: "/#uslugi" },
+    { label: "O Nas", href: "/#o-nas" },
+    { label: "Realizacja", href: "/#realizacja" },
+    { label: "Kontakt", href: "/#kontakt" },
 ]
