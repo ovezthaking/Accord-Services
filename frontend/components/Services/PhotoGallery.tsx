bd5920f751bbe923512e36a68e67f00a9a32e1ed
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-03-02 02:07:41 +0100

    self made working gallery unstyled - thinking about search for gallery components

--- frontend/components/Services/PhotoGallery.tsx
+++ frontend/components/Services/PhotoGallery.tsx
@@ -7,8 +7,31 @@ import { photoGalleryProps } from "./types";
 export default function PhotoGallery({images}: photoGalleryProps) {
     const [activeIndex, setActiveIndex] = useState<number>(0)
 
+    const onDecrease = () => {
+        const nextIndex = activeIndex - 1
+        console.log(activeIndex)
+        if(nextIndex < 0) {
+            setActiveIndex(images.length - 1)
+        }
+        else {
+            setActiveIndex(nextIndex)
+        }
+    }
+
+    const onIncrease = () => {
+        const nextIndex = activeIndex + 1
+        console.log(activeIndex)
+        if(nextIndex > images.length - 1) {
+            setActiveIndex(0)
+        }
+        else {
+            setActiveIndex(nextIndex)
+        }
+    }
+
     return (
         <>
+            <button onClick={() => onDecrease()}>-</button>
             <Image 
                 key={activeIndex}
                 width={800}
@@ -16,6 +39,7 @@ export default function PhotoGallery({images}: photoGalleryProps) {
                 src={images[activeIndex].src}
                 alt={images[activeIndex].alt}
             />
+            <button onClick={() => onIncrease()}>+</button>
         </>
     )
 }
