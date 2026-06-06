9c0f7a612c65e53993a923b51e8ab7fbae256cca
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-06-06 19:29:23 +0200

    threads for mail sending

--- contact/utils/send_mail.py
+++ contact/utils/send_mail.py
@@ -5,9 +5,9 @@ def send_contact_mail(mail_data: dict):
     send_mail(
         subject=mail_data.get('full_name') + ' - ' + mail_data.get('services'),
         message='Otrzymano wiadomość:\n' +
-        f'Imię i nazwisko: {mail_data.get('full_name')}\n' +
-        f'Adres e-mail: {mail_data.get('email')}\n' +
-        f'Numer telefonu: {mail_data.get('phone_number')}\n' +
+        f"Imię i nazwisko: {mail_data.get('full_name')}\n" +
+        f"Adres e-mail: {mail_data.get('email')}\n" +
+        f"Numer telefonu: {mail_data.get('phone_number')}\n" +
         'Treść wiadomości: \n' +
         mail_data.get('description'),
         from_email="accordstrona@gmail.com",
