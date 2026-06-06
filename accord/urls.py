2f43c425dcfd45b38a865ab8c248a39d5a6668c8
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-06-06 05:53:14 +0200

    redirect to admin

--- accord/urls.py
+++ accord/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/contacts', include('contact.api.urls'))
+    path('api/contacts', include('contact.api.urls')),
+    path('', RedirectView.as_view(url='/admin/', permanent=False))
 ]
