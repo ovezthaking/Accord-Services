05b9dc842fa62ea535991357a3d3813b89d761c6
Oliwer Urbaniak <oliwerx12@gmail.com>
2026-06-07 00:33:37 +0200

    change direction of view site button to frontend

--- contact/admin.py
+++ contact/admin.py
@@ -5,3 +5,5 @@ from .models import Contact, ContactStatus, EmailRecipient
 admin.site.register(Contact)
 admin.site.register(ContactStatus)
 admin.site.register(EmailRecipient)
+
+admin.site.site_url = "https://accord.opole.pl"
